5ac516864521a67e7b33a439f7a760b60ff96ec2
c.fuss <c.fuss@datatrain.de>
2020-05-07 14:57:43 +0200

    update working setup

--- srv-proxy/index.js
+++ srv-proxy/index.js
@@ -8,7 +8,8 @@ const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
 const host = "0.0.0.0";
 const port = process.env.PORT || 4004;
-const target = process.env.srv_api_url || "http://localhost:8000";
+const target = process.env.srv_api_url || "http://localhost:8080";
+const model = process.env.srv_api_url ? "./gen/csn.json" : "./srv-proxy/gen/csn.json";
 
 (async () => {
   const app = express();
@@ -17,7 +18,7 @@ const target = process.env.srv_api_url || "http://localhost:8000";
   process.env.XS_APP_LOG_LEVEL = "warning";
   app.use(proxy({
     path: "v2",
-    model: "./gen/csn.json",
+    model: model,
     standalone: true,
     target: target,
     services: {
